9d5adfdde06cc3ffcb04b2b459f6f00d3ac870c2
Pavel Kalinkin <pnzazfof@gmail.com>
2022-05-03 00:58:53 +0300

    day 8 part 2 solved

--- aoc/days/day8/solution.py
+++ aoc/days/day8/solution.py
@@ -1,3 +1,4 @@
+import numbers
 from typing import List
 
 
@@ -22,4 +23,90 @@ def part1(file_path: str) -> int:
 
 
 def part2(file_path: str) -> int:
-    raise NotImplemented('No code for day8 part 2')
+    summ = 0
+    for line in read(file_path):
+        raw_alphabet, raw_numbers = line.split('|')
+        decoder = Decoder(
+            [x.strip() for x in raw_alphabet.strip().split(' ')],
+            [x.strip() for x in raw_numbers.strip().split(' ')]
+        )
+        summ += decoder.get_decoded()
+    
+    return summ
+
+
+class Decoder:
+    def _len_to_number(self, len):
+        if len == 5:
+            return [2, 3, 5]
+        elif len == 6:
+            return [0, 6, 9]
+        elif len == 2:
+            return [1]
+        elif len == 4:
+            return [4]
+        elif len == 3:
+            return [7]
+        return [8]
+
+    def __init__(self, alpabet, target_number):
+        self.target = target_number
+        display = {segment: None for segment in 'abcdefg'}
+        self.numbers = {num: [] for num in range(0, 10)}
+
+        for num in alpabet:
+            for candidate in self._len_to_number(len(num)):
+                self.numbers[candidate].append(num)
+
+        display['a'] = list(set(*self.numbers[7]).difference(set(*self.numbers[1])))
+        display['b'] = list(set(*self.numbers[4]).difference(set(*self.numbers[1])))
+        display['c'] = list(set(*self.numbers[1]))
+        display['d'] = list(set(*self.numbers[4]).difference(set(*self.numbers[1])))
+        display['e'] = []
+        display['f'] = list(set(*self.numbers[1]))
+        display['g'] = []
+
+        self.numbers[5] = list(filter(lambda x: set(display['b']).issubset(set(x)), self.numbers[5]))
+        
+        c_f_values = display['c']
+        if c_f_values[0] in self.numbers[5][0]:
+            display['f'] = c_f_values[0]
+            display['c'] = c_f_values[1]
+        else:
+            display['f'] = c_f_values[1]
+            display['c'] = c_f_values[0]
+        
+        self.numbers[2] = list(
+            filter(
+                lambda x: display['c'] in x and display['f']not in x,
+                list(set(self.numbers[2]) - set(self.numbers[5]))
+                )
+            )
+        
+        self.numbers[3] = list(set(self.numbers[3]) - set(self.numbers[2]) -  set(self.numbers[5]))
+
+        self.numbers[6] = list(
+            filter(
+                lambda x: display['c'] not in x,
+                list(set(self.numbers[6]) - set(self.numbers[8]))
+                )
+            )
+
+        backup = set(display['d'])
+        display['d'] = list(set(*self.numbers[2]).intersection(set(display['d'])))
+        display['b'] = list(backup.difference(set(display['d'])))
+
+        self.numbers[9] = list(
+            filter(
+                lambda x: display['d'][0] in x,
+                list(set(self.numbers[9]) - set(self.numbers[6]))
+                )
+            )
+
+        self.numbers[0] = list(set(self.numbers[0]) - set(self.numbers[6])- set(self.numbers[9]))
+
+    def get_decoded(self):
+        tmp = ""
+        for t in self.target:
+            tmp += [str(key) for key, value in self.numbers.items() if set(t) == set(*value)][0]
+        return int(tmp)
--- notes/day8.md
+++ notes/day8.md
@@ -4,3 +4,64 @@ Part 1 is pretty easy and can be done by just reading puzzle description.
 In short: Numbers 1, 4, 7 and 8 unique in terms of number of active segments.
 
 Part 2 requires us to decode that segments to get final result
+
+```
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+```
+
+On the start we have knowledge of all 10 numbers, but not sure what is what.
+
+Algorithm is based on segments overlapping.
+
+We already knew some numbers (1, 4, 7 and 8) and we hate to get lest and detect which number is which display segment. This is needed for detection more complex numbers.
+
+We initialize segments with next data:
+segment `a` - all from number 7 minus all from number 1. 
+segment `b` - all from number 4 minus all from number 1. 
+segment `c` - all from number 1
+segment `d` - all from number 4 minus all from number 1. 
+segment `e` - empty
+segment `f` - all from number 1
+segment `g` - empty
+
+There are 6 numbers left, grouped by segments length.
+
+We start with 5-segment group.
+
+We can filter number 5 by validation that all from segments `b` and `d`.
+After what we can confirm which segment in number 1 in charge for `c` and `f` segments
+
+Next we find numbers 3 and 2. 
+In number 3 no `f` segment and segment `c` is presented.
+Number 2 is last from 5-segments group.
+
+In remaining group we fan filter number 6 as number 8 minus segment `c`
+
+Now we have to find segment `d` to detect remaining numbers.
+We can find `d` as intersection of number 2 and what we think is `d`.
+With it we can find 9 as 6-segment number with segment `d` and remaining number would by zero.
+
+Tricky part of puzzle is over.
+
+Performance:
+
+|   DAY |   MEM 1, Kb | TIME 1         |   MEM 2, Kb | TIME 2         |
+|-------|-------------|----------------|-------------|----------------|
+|     8 |       41.72 | 0:00:00.001595 |      41.584 | 0:00:00.025164 |
+
